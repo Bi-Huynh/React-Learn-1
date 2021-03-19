d44b040410e09843d0be65254f0a86a0d10ff23d
=Bi-Huynh <=huynhtrungduc1231998@gmail.com>
2021-03-19 23:41:33 +0700

    update todoList v1

--- src/components/Todo/Card/Card.js
+++ src/components/Todo/Card/Card.js
@@ -3,59 +3,71 @@ import TodoHeader from '../Header/Header.js';
 import TodoList from '../List.js';
 import './Card.css';
 
+const isCheckedAll = (todoItems) => {
+    for (const item of todoItems) {
+        if (!item.isComplete) {
+            return false;
+        }
+    }
+    return true;
+};
+
 const TodoCard = () => {
     const [todoItems, setTodoItems] = useState([
-        { title: 'Mua bim bim', isComplete: false },
-        { title: 'Mua gạo', isComplete: false },
-        { title: 'Đi đổ xăng', isComplete: false },
+        { id: 1, title: 'Mua bim bim', isComplete: false },
+        { id: 2, title: 'Mua gạo', isComplete: false },
+        { id: 3, title: 'Đi đổ xăng', isComplete: false },
     ]);
+
+    const [isCompleteAllItem, setIsCompleteAllItem] = useState(false);
 
     const onKeyEnter = (event) => {
         let contentTodo = event.target.value.trim();
         if (contentTodo !== '' && event.key === 'Enter') {
-            setTodoItems([
-                { title: contentTodo, isComplete: false },
-                ...todoItems,
-            ]);
+            let newTodoItems = JSON.parse(JSON.stringify(todoItems));
+            newTodoItems.unshift({ title: contentTodo, isComplete: false });
+            setTodoItems(newTodoItems);
             event.target.value = '';
+
+            isCheckedAll(newTodoItems)
+                ? setIsCompleteAllItem(true)
+                : setIsCompleteAllItem(false);
+            // if (isCheckedAll(newTodoItems)) {
+            //     setIsCompleteAllItem(true);
+            // }
         }
     };
-
-    // const onItemClicked = (item, index) => {
-    //     return (event) => {
-    //         let isComplete = item.isComplete;
-    //         setTodoItems([
-    //             ...todoItems.slice(0, index),
-    //             { ...item, isComplete: !isComplete },
-    //             ...todoItems.slice(index + 1),
-    //         ]);
-    //     };
-    // };
 
     const onItemClicked = (item) => {
         let index = todoItems.indexOf(item);
         if (index < 0) return;
 
         let isComplete = item.isComplete;
-        let newTodoItems = [...todoItems];
+        let newTodoItems = JSON.parse(JSON.stringify(todoItems));
         newTodoItems[index].isComplete = !isComplete;
         setTodoItems(newTodoItems);
+
+        // kiểm tra xem có phải tất cả đã check hay không
+        // nếu không thì bỏ check all và ngược lại
+        isCheckedAll(newTodoItems)
+            ? setIsCompleteAllItem(true)
+            : setIsCompleteAllItem(false);
+        // if (isCheckedAll(newTodoItems)) {
+        //     setIsCompleteAllItem(true);
+        // }
     };
 
     const onAllComplete = () => {
-        // setTodoItems([...todoItems]);
-        let isComplete = !isComplete || false;
-        let newTodoItems = [...todoItems];
-        newTodoItems = newTodoItems.map(
-            (item) => (item.isComplete = !isComplete)
-        );
-
-        console.log(newTodoItems);
+        let newTodoItems = JSON.parse(JSON.stringify(todoItems));
+        newTodoItems.map((item) => (item.isComplete = !isCompleteAllItem));
+        setIsCompleteAllItem(!isCompleteAllItem);
+        setTodoItems(newTodoItems);
     };
 
     return (
         <div className="todo-card">
             <TodoHeader
+                isCompleteAllItem={isCompleteAllItem}
                 onAllComplete={onAllComplete}
                 onKeyEnter={onKeyEnter}
             ></TodoHeader>
--- src/components/Todo/Header/Header.js
+++ src/components/Todo/Header/Header.js
@@ -1,9 +1,10 @@
-import React, { useRef, useEffect } from 'react';
-import './Header.css';
+import React, { useEffect, useRef } from 'react';
+import checkAllComplete from '../../../image/icon/check-all-complete-2.svg';
 import checkAll from '../../../image/icon/check-all.svg';
+import './Header.css';
 
 function TodoHeader(props) {
-    const { onAllComplete, onKeyEnter } = props;
+    const { isCompleteAllItem, onAllComplete, onKeyEnter } = props;
     const inputEl = useRef(null); // khởi tạo ref
     // sử dụng ref để lấy element mong muốn
     // sử dụng effect để thực hiện 1 cái gì đó sau khi DOM đã được render xong
@@ -11,12 +12,11 @@ function TodoHeader(props) {
     useEffect(() => inputEl.current.focus());
     // sử dụng effect để cứ mỗi lần render lại view sau khi state todoItems được update
     // và khi DOM được render xong thì nó sẽ cho ô input hiển thị con trỏ nhập liệu bên trong
-
     return (
         <div className="card-header">
             <img
                 className="img"
-                src={checkAll}
+                src={isCompleteAllItem ? checkAllComplete : checkAll}
                 alt="icon down"
                 onClick={onAllComplete}
             />
